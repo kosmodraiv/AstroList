0c4d8dc675edac04a7bbe13bb5c66ca81056312e
Misha <wd.volovik322@gmail.com>
2023-06-10 20:41:49 +0300

    validation

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -4,28 +4,26 @@ import { Link } from 'react-router-dom';
 const Login = () => {
 	return (
 		<main>
-			<section className="publish-tip sign-form">
-				<form>
-					<fieldset>
-						<legend>Вход</legend>
-						<label className="label">
-							<input placeholder="Электропочта" type="text" name="email" />
-						</label>
-						<label className="label">
-							<input placeholder="Пароль" type="password" name="name" />
-						</label>
-						<button
-							className="button-login"
-							type="submit"
-						>
-							<Link to="/display">Войти</Link>
-						</button>
-					</fieldset>
-					<p>
-						Если у вас до сих пор нет учётной записи, вы можете <Link to="/register">зарегистрироваться</Link>.
-					</p>
-				</form>
-			</section>
+  			<section className="publish-tip sign-form">
+    			<form>
+      				<fieldset>
+        				<legend>Вход</legend>
+       					<label className="label">
+          					<input placeholder="Электропочта" type="text" name="email" />
+       					</label>
+       					<label className="label">
+          					<input placeholder="Пароль" type="password" name="password" />
+        				</label>
+       					<button className="button-login" type="submit">
+          					<Link to="/display">Войти</Link>
+        				</button>
+      				</fieldset>
+      				<p>
+        				Если у вас до сих пор нет учётной записи, вы можете {" "}
+        				<Link to="/register">зарегистрироваться</Link>.
+      				</p>
+    			</form>
+  			</section>
 		</main>
 	);
 };
